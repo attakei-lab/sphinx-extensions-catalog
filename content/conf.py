3b32a9dd51d113cbe060e1bf1913386caba33792
Kazuya Takei <myself@attakei.net>
2025-05-02 14:36:42 +0900

    content: Initial content

---
+++ content/conf.py
@@ -0,0 +1,14 @@
+# -- Project information
+project = "Sphinx extensions Catalog"
+copyright = "2025, Kazuya Takei"
+author = "Kazuya Takei"
+release = "0.0.0"
+
+# -- General configuration
+extensions = []  # type: ignore[var-annotated]
+templates_path = ["_templates"]
+language = "ja"
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
