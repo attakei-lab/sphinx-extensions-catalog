9d3d2d36689f0ae582a1501848f9148a3407b60f
Kazuya Takei <myself@attakei.net>
2025-06-11 01:19:25 +0900

    feat: Set up as sample

--- content/conf.py
+++ content/conf.py
@@ -1,8 +1,10 @@
 # -- Project information
-project = "Sphinx extensions Catalog"
+project = "Sphinx拡張 ミニカタログ"
 copyright = "2025, Kazuya Takei"
 author = "Kazuya Takei"
 release = "0.0.0"
+release = "〜Sphinxユーザーによる、Sphinx拡張コレクションノート……のサンプル版〜"
+release = "〜サンプル版〜"
 
 # -- General configuration
 extensions = []  # type: ignore[var-annotated]
@@ -12,3 +14,7 @@ language = "ja"
 # -- Options for HTML output
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+latex_elements = {
+    "releasename": " ",
+}
